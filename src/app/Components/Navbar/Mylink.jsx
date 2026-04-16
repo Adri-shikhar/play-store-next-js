c9a27976bd830ba6a9081c02d4548d6d72aabd82
Adri-shikhar <adrishikharbarua77452@gmail.com>
2026-04-16 21:14:45 +0600

    Made navbar and footer

--- src/app/Components/Navbar/Mylink.jsx
+++ src/app/Components/Navbar/Mylink.jsx
@@ -8,20 +8,8 @@ const Mylink = ({ href, label, className = '' }) => {
     const isActive = pathname === href;
 
     return (
-        <Link
-            href={href}
-            className={`
-                px-1 py-1 rounded-lg text-base font-medium transition-all duration-300
-                ${isActive
-                    ? 'bg-purple-600 text-white shadow-md'
-                    : 'text-gray-700 hover:bg-gray-100'
-                }
-                ${className}
-            `}
-        >
-            {label}
-        </Link>
-    );
+        <>
+        </>
+    )
 };
-
 export default Mylink;
